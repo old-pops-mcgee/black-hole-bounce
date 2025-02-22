700b2f71edbd1ec1901d2c4f0acc607c80ee8a99
Dennis Lynch <oldpopsmcgeegaming@gmail.com>
2025-02-21 21:39:31 -0500

    Updates mechanics

--- entities/ball.py
+++ entities/ball.py
@@ -3,48 +3,42 @@ import math
 import pyray
 
 class Ball:
+    MAX_SPEED = 5.0
+
     def __init__(self, game, pos, radius, color):
         self.game = game
         self.pos = list(pos) # x,y
         self.radius = radius
         self.color = color
 
-        self.velocity = [0.0, 5.0] # X velocity, Y velocity
+        self.velocity = [0.0, 0.0] # X velocity, Y velocity
 
     def render(self):
         pyray.draw_circle(int(self.pos[0]), int(self.pos[1]), self.radius, self.color)
 
     def update(self):
-        # Check if we're off screen
-        if self.pos[1] > self.game.WINDOW_HEIGHT:
-            self.game.game_over = True
-            return
 
         # Check to see if we've scored
         for point in self.game.point_list:
             if pyray.check_collision_circles(self.pos, self.radius, point.pos, point.radius):
                 self.game.score_point(point)
 
-        # Adjust for impulse from paddle
-        if pyray.check_collision_circle_rec(self.pos, self.radius, self.game.paddle.get_pyray_rec()):
-            self.velocity[1] = -10.0
-
         # Adjust if we are colliding with the walls
-        if self.pos[1] <= 0:
-            self.velocity[1] = 10.0
+        if self.pos[1] < 0 or self.pos[1] > self.game.WINDOW_HEIGHT:
+            self.velocity[1] *= -1
 
-        if self.pos[0] <= 0:
-            self.velocity[0] = 10.0
+        if self.pos[0] < 0 or self.pos[0] > self.game.WINDOW_WIDTH:
+            self.velocity[0] *= -1
 
-        if self.pos[0] >= self.game.WINDOW_WIDTH:
-            self.velocity[0] = -10.0
+        # Check to see if we've been crushed
+        for black_hole in self.game.black_hole_list:
+            if pyray.check_collision_circles(self.pos, self.radius, black_hole.pos, black_hole.death_radius):
+                self.game.game_over = True
+                return
 
         # Calculate new position
         self.pos[0] += self.velocity[0]
         self.pos[1] += self.velocity[1]
-
-        # Calculate velocity updates from downwards gravity
-        self.velocity[1] += 0.1 # Natural gravity + impact from black hole
 
         # Calculate velocity updates from black hole gravity
         for black_hole in self.game.black_hole_list:
@@ -53,5 +47,5 @@ class Ball:
             self.velocity[1] += y_force
 
         # Cap velocities so we don't get TOO crazy
-        self.velocity[0] = min(10.0, self.velocity[0])
-        self.velocity[1] = min(10.0, self.velocity[1])
+        self.velocity[0] = min(self.MAX_SPEED, max(-self.MAX_SPEED, self.velocity[0]))
+        self.velocity[1] = min(self.MAX_SPEED, max(-self.MAX_SPEED, self.velocity[1]))
--- entities/black_hole.py
+++ entities/black_hole.py
@@ -2,15 +2,25 @@ import math
 import pyray
 
 class BlackHole:
-    STANDARD_FORCE = 4000.0
+    STANDARD_FORCE = 2500.0
+    FUDGE_FACTOR = 3.5
+    DECAYING_FORCE_ADDER = 20.0
+    DECAY_RATE = 0.25
     def __init__(self, game, pos, radius):
         self.game = game
         self.pos = list(pos)
         self.radius = radius
         self.force = self.STANDARD_FORCE
+        self.level = 1
+        self.death_radius = 0.2*self.radius
 
     def update(self):
-        pass # TODO: Add animation effects on the black hole
+        # TODO: Add animation effects on the black hole
+        self.radius -= self.DECAY_RATE
+        self.death_radius -= 0.2*self.DECAY_RATE
+        self.force += self.DECAYING_FORCE_ADDER
+        if self.radius < self.DECAY_RATE:
+            self.game.black_hole_list.remove(self)
 
     def render(self):
         # Render hole from out to in: RED -> ORANGE -> YELLOW -> BLACK
@@ -19,23 +29,19 @@ class BlackHole:
         pyray.draw_circle(int(self.pos[0]), int(self.pos[1]), self.radius * 0.9, pyray.YELLOW)
         pyray.draw_circle(int(self.pos[0]), int(self.pos[1]), self.radius * 0.85, pyray.BLACK)
 
-    def calculate_force_on_ball(self, ball_x, ball_y):
-        fudge_factor = 0 if self.pos != ball_x else 1
-        angle = math.atan((self.pos[1] - ball_y) / (self.pos[0] - ball_x - fudge_factor))
+        # Render 'death radius' that crushes ball
+        pyray.draw_circle(int(self.pos[0]), int(self.pos[1]), self.death_radius, pyray.DARKPURPLE)
 
+    def calculate_force_on_ball(self, ball_x, ball_y):
+        angle = math.atan2((self.pos[1] - ball_y), (self.pos[0] - ball_x))
         dis = math.sqrt(math.pow(self.pos[1] - ball_y, 2) + math.pow(self.pos[0] - ball_x, 2))
 
         # Normal equation is G*(mass1*mass2)/dis^2
         # Gravity being applied separately, need to calculate component of force on each end
         # Hardcode force as something over the distance squared
-        g_force = self.force / math.pow(dis, 2)
-        x_force = abs(math.cos(angle) * g_force)
-        if self.pos[0] < ball_x:
-            # black hole to left of ball, so pull ball left
-            x_force *= -1
+        # Add a 'fudge factor' to give players more reaction time
+        g_force = self.force / (self.FUDGE_FACTOR*math.pow(dis, 2))
+        x_force = math.cos(angle) * g_force
         y_force = math.sin(angle) * g_force
-        if self.pos[1] < ball_y:
-            # black hole above ball, so pull ball up
-            y_force *= -1
 
         return x_force, y_force
